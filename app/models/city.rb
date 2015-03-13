afcadc9efaba7bc4595078cc791c9a7ba6586a85
Ernesto Valenzuela <ernesto.valenzuela@heptagram.com.mx>
2015-03-12 19:01:28 -0600

    Relaciones entre tablas, faltan publicaciones y destino y origen

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,5 @@
 class City < ActiveRecord::Base
-  belongs_to :states
+  belongs_to :state
+  has_many :users
+  has_and_belongs_to_many :publications
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ActiveRecord::Base
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+    
+    has_many :inboxes
+    belongs_to :city
+    has_and_belongs_to_many :services
+    has_and_belongs_to_many :trucks
+    has_and_belongs_to_many :loads
+    has_and_belongs_to_many :ratings
 end
