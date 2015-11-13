669c7318ce5a6f9bb0a8656ee6e5ab4a2b84fe5a
Ernesto Valenzuela <programminghack@gmail.com>
2015-11-13 14:33:04 -0600

    Agregando seleccionar todos los transportistas

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -317,6 +317,14 @@
         $(".thevalue").val("");
         $(".form-ocult").show();
     })
-        
+    
+    $('#select_all').change(function() {
+        var checkboxes = $(this).closest('form').find(':checkbox');
+        if($(this).is(':checked')) {
+            checkboxes.prop('checked', true);
+        } else {
+            checkboxes.prop('checked', false);
+        }
+    });
    
 })()
